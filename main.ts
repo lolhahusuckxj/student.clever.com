a5b019ca2a52bc9dd3286dba42d784b8d914c7fe
lolhahusuckxj <ezzuracalley46@gmail.com>
2026-05-07 17:51:47 -0700

    Refactor to use async file read for HTML

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
-import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
+const html = await Deno.readTextFile("./index.html");
 
-serve((req) => {
-  const html = Deno.readTextFileSync("./index.html");
+Deno.serve((req) => {
   return new Response(html, {
-    headers: { "content-type": "text/html" },
+    headers: { "content-type": "text/html; charset=utf-8" },
   });
 });
